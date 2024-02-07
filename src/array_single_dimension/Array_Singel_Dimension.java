620b4fa1ff1125a86c2a8d4db17d7722d7b58f5e
sokoeurn88 <sokoeurn.chhaya@gmail.com>
2024-02-07 11:23:55 -0600

    array

---
+++ src/array_single_dimension/Array_Singel_Dimension.java
@@ -0,0 +1,25 @@
+package array_single_dimension;
+
+public class Array_Singel_Dimension {
+
+	public static void main(String[] args) {
+		
+		/* Single dimension array, fix array, know size before*/
+		int a[] = new int[5];
+		a[0]=100;
+		a[1]=200;
+		a[2]=300;
+		a[3]=400;
+		a[4]=500;
+		System.out.println(a[1]);
+		
+		/* Multi dimension array, Dynamic array*/
+		int b[]= {10,20,30,40,50};
+		System.out.println(b[1]);
+		System.out.println("Array length is : "+b.length);
+				
+				
+				
+	}
+
+}
---
+++ src/java_webdrivermanager/Dev_Tool_With_Untruted_Website.java
@@ -0,0 +1,14 @@
+package java_webdrivermanager;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Dev_Tool_With_Untruted_Website {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+//		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
