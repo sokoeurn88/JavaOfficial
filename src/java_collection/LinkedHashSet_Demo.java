00ee1f0f4ce01fb4b98cbf6d1469236c99db2108
sokoeurn88 <sokoeurn.chhaya@gmail.com>
2024-02-11 22:18:19 -0600

    Queue interface and PriorityQueue

---
+++ src/java_collection/LinkedHashSet_Demo.java
@@ -0,0 +1,22 @@
+package java_collection;
+
+
+public class LinkedHashSet_Demo {
+
+	public static void main(String[] args) {
+		
+		/*default capacity is 16, no duplicate object, insertion order is preserved, 
+		 * Underlying data structure is HashTable + LinkedList
+		 * all methods used in HashSet can be used in LinkedHashSet */
+		
+		/*not work, Do not know a reason*/
+//		LinkedHashSet_Demo <Integer> lhs = new LinkedHashSet_Demo<Integer>();
+//		lhs.add(100);
+		
+		
+		
+	}
+
+	
+
+}
---
+++ src/java_collection/Queue_PriorityQueue_LinkedList.java
@@ -0,0 +1,53 @@
+package java_collection;
+
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+public class Queue_PriorityQueue_LinkedList {
+
+	public static void main(String[] args) {
+		
+		/*Homogeneous data type, when element prior to processing, insertion order preserved, duplication is allowed
+		 * */
+		PriorityQueue pq = new PriorityQueue();
+		pq.add('a');
+		pq.add('b');
+		pq.add('b');
+		pq.add('c');
+		pq.offer('s');
+		pq.offer('s');
+		System.out.println(pq);
+		
+		/*element() and peek() are for get head element if the PriorityQueue is not empty,
+		 * if PriorityQueue is empty, then element() will return exception
+		 * if PriorityQueue is empty, them peek() will return null */ 
+		System.out.println(pq.element());
+		System.out.println(pq.peek());
+		
+		/*poll() and remove() are for getting a head element and remove it from PriorityQueue if PriorityQueue is not empty
+		 * if PriorityQueue is empty, remove() will return exception
+		 * if PriorityQueue is empty, poll() will return null
+		 * */
+		System.out.println(pq.remove());
+		System.out.println(pq);
+		
+		System.out.println(pq.poll());
+		System.out.println(pq);
+		
+		for(Object x:pq) {
+			System.out.println(x);
+		}
+		
+		Iterator it = pq.iterator();
+		while(it.hasNext()) {
+			System.out.println("=>"+it.next());
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
