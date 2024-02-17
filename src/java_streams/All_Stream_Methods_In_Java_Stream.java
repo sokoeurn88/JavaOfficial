1579d44567d8293b2bf4dbb612c09877b52a9f27
sokoeurn88 <sokoeurn.chhaya@gmail.com>
2024-02-17 09:50:52 -0600

    Java Stream min() max() reduce() toArray()

--- src/java_streams/All_Stream_Methods_In_Java_Stream.java
+++ src/java_streams/All_Stream_Methods_In_Java_Stream.java
@@ -2,6 +2,7 @@ package java_streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class All_Stream_Methods_In_Java_Stream {
@@ -25,6 +26,40 @@ public class All_Stream_Methods_In_Java_Stream {
 		/*count*/
 		long count = transport.stream().distinct().count();
 		System.out.println(count);
+		
+		/*limit()*/
+		List<String> limstransport = transport.stream().limit(2).collect(Collectors.toList());
+		System.out.println(limstransport);
+		transport.stream().limit(3).forEach(System.out::println);
+		
+		/*New Example*/
+		List<Integer>num= Arrays.asList(1,2,3,4,5,6,7,8,9,0);
+				
+		/*min() is non-terminal method*/
+		Optional<Integer> minNum = num.stream().min((value1,value2)->{return value1.compareTo(value2);});
+		System.out.println(minNum.get());
+		
+		/*max()*/
+		Optional<Integer> maxNum = num.stream().max((value1,value2)->{
+			return value1.compareTo(value2);
+		});
+		System.out.println(maxNum.get());
+		
+		/*reduce()*/
+		Optional<Integer> reduceNum = num.stream().reduce((value,combinedvalue)->{
+			return combinedvalue+value;
+		});
+		System.out.println(reduceNum.get());
+		
+		
+		/*toArray()*/
+		Object[] numArray = num.stream().toArray();
+		for(Object n:numArray) {
+			System.out.println(n);
+		}
+		
+		
+		
 
 	}
 
